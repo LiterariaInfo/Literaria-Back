6b15f9e38295e53a82f187dcf527203949a31c88
Karo8870 <cazacuchristian@gmail.com>
2023-12-01 18:59:50 +0200

    Header animation works!

--- multer/upload.ts
+++ multer/upload.ts
@@ -1,19 +1,14 @@
 import multer from 'multer';
-import path from 'path';
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    console.log(333333)
     cb(null, './uploads');
-    console.log("mircea")
   },
   filename(req, file, cb) {
-    console.log(777777)
     cb(
       null,
       `${Date.now()}-${Math.random().toString(36).substring(2, 8)}`
     );
-    console.log("cristea")
   }
 });
 
